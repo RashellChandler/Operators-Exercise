8e5f85ca95345f3ba8f421489c91c6efbb23f335
RashellChandler <rashell.chandler@yahoo.com>
2023-08-05 11:44:50 -0500

    Pushing exercise 1

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            // For division, create two integer variables called a and b.
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine(a + " / " + b + " is " + quotient + " remainder " + remainder);
+            Console.WriteLine($"{a}/ {b} is {quotient} remainder {remainder}");
+                 
+
+
+
+
     }
+
 }
